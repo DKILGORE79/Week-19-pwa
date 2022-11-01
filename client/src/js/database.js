900cb43e64e850108e87c2899afbba5012c14002
Derek Kilgore <derek.kilgore@gmail.com>
2022-10-31 22:15:40 -0500

    Pushing to Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const db = await openDB('jate', 1);
@@ -23,8 +22,6 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 }
 
-
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   const db = await openDB('jate', 1);
@@ -35,4 +32,5 @@ export const getDb = async () => {
   console.log('result.value', result[0].jate);
   return result[0].jate;
 }
+
 initdb();
